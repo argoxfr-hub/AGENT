7c45192109f0c2a6d2eca0bae890d79c0a671456
argoxfr-hub <argox.fr@gmail.com>
2026-05-28 11:27:04 +0000

    fix: disable TLS verification before module load

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,4 @@
-import 'reflect-metadata';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
